8c976a3e4a1c23a6ea5b7b8f9637aca9f07db2f9
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-11-22 04:28:41 +0000

    Updated Gamecom.js

--- myapp/src/components/Gamecom.js
+++ myapp/src/components/Gamecom.js
@@ -18,4 +18,30 @@ export default function Gamecom(){
             setResult("Lose");
         }
     }
+
+    const restartGame=()=>{
+        setTargetAnimal(getRandomAnimal());
+        setResult(null);
+    };
+
+    return(
+        <>
+        <div className="game-page">
+            <div>
+                <h1>Animal Matching Game</h1>
+            </div>
+            <div>
+                <div>
+                    {result && (
+                        <div className="result">
+                            <h2>Result</h2>
+                            <h2>{result}</h2>
+                            <button onClick={restartGame}>Play Again</button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+        </>
+    )
 }
