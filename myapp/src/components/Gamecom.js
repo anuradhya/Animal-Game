00a5369611aed93a0ecd6d516dfc66921a1cf2e5
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-11-22 04:24:19 +0000

    Modifed Gamecom.js

--- myapp/src/components/Gamecom.js
+++ myapp/src/components/Gamecom.js
@@ -1,3 +1,21 @@
 import {useState} from "react";
 import animals from "../AnimalsDb";
+import "../assets/gamecom.css";
 
+export default function Gamecom(){
+    const [targetAnimal,setTargetAnimal]=useState(getRandomAnimal());
+    const [result,setResult]=useState(null);
+
+    function getRandomAnimal(){
+        const randomIndex=Math.floor(Math.random()*animals.length);
+        return animals[randomIndex];
+    }
+
+    const handleAnimalClick=(id)=>{
+        if (id===targetAnimal.id){
+            setResult("Win");
+        }else{
+            setResult("Lose");
+        }
+    }
+}
